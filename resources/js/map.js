bc8011a218e0dd67c7f22e6a64a90b43b18df196
postolenko <denis.postolenko@gmail.com>
2015-06-25 20:03:11 +0300

    Sedona Hotels

--- resources/js/map.js
+++ resources/js/map.js
@@ -14,13 +14,13 @@
 //     title:"Hello World!" 
 // });
 
-var map;
-function initialize() {
-  map = new google.maps.Map(document.getElementById('map-canvas'), {
-    zoom: 8,
-    center: {lat: -34.397, lng: 150.644}
-  });
-}
+// var map;
+// function initialize() {
+//   map = new google.maps.Map(document.getElementById('map-canvas'), {
+//     zoom: 8,
+//     center: {lat: -34.397, lng: 150.644}
+//   });
+// }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+// google.maps.event.addDomListener(window, 'load', initialize);
 
