b562165cdc681642c8eb3a989498555c9d2374d9
postolenko <denis.postolenko@gmail.com>
2015-07-07 01:32:20 +0300

    Sedona Hotels

--- resources/js/switch_pages.js
+++ resources/js/switch_pages.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
 
 
 			$("#map-canvas").css({"width": ($(window).width() + ( $(window).width()/100 )*20 ) + "px",
-				"margin-left": -$(window).width()/100*20 + "px"});
+								  "margin-left": -1 * $(window).width()/100*20 + "px"});
 
 			$("#map-canvas").height($(window).height());
 
