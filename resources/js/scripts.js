b289e8bfee94ca8347a5e9e699707f53758cc17e
postolenko <denis.postolenko@gmail.com>
2015-07-16 15:51:48 +0300

    Sedona Hotels

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -416,6 +416,59 @@ var countScrollForHeader = 0;
 
 
 
+
+
+
+	var indexMenu;
+	var namePage;
+
+	$(".navigation li").click(function() {
+
+		indexMenu = $(this).index();
+		
+		namePage = $(".nav-link:eq("+indexMenu+")").text();
+		
+		if(namePage == "Гостиницы") {
+
+			$(".information-page").fadeOut(300);
+
+			$("footer").css({"top":"0px"});
+
+			$(".hotels-page").css({
+				"display":"block",
+				"position":"relative"
+			});
+
+			$(".hotels-page").animate({"left":"0"},1000);
+
+			
+		}
+
+		if(namePage == "Информация") {
+
+			$(".hotels-page").css({"display":"none"});
+
+			$(".information-page").fadeIn(1000);
+
+			$("footer").css({"top":"450px"});
+
+			$(".hotels-page").css({"left":"100%"});
+
+			// -----------------------
+
+			showMap();
+			$("#map-canvas").css({"width": ($(window).width() + ( $(window).width()/100 )*20 ) + "px",
+								  "margin-left": -$(window).width()/100*20 + "px"});
+
+			$("#map-canvas").height($(window).height());
+
+			// -----------------------
+
+		}
+
+	});
+
+
     
 
 });
